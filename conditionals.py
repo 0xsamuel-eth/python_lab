345ac74cdd99f18fbd770bde897783e5cc40672d
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-12 13:37:57 -0400

    Added nested loops to iterate price for each menu item

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+#Let's start a pizza parlor together! We're going to build a pizza parlor using some new Python programming concepts!!
+
+#Let's build robot Cashier
+
+#print("\nHello, welcome to 0xSamuel Pizza!!!!!!!\n")
+
+#name = input("What is your name?\n")
+
+#if name == "Ben":
+#    print("You're not welcome here Evil Ben!! Get out!!")
+#else:
+#    print("Hello " + name + ", thank you so much for coming in today.\n\n")
+
+if 4 > 3:
+    print("Yep, it's true!!")
+    print("It's still true!!")
+else:
+    print("Nope, not true!!")
--- variables.py
+++ variables.py
@@ -10,22 +10,39 @@ name = input("What is your name?\n")
 
 #Greet customer w/ name and thank them for coming
 if name == "Ben":
-    print("You're not welcome here Evil Ben!! Get out!!")
-    exit()
+    evil_status = input("Are you evil?\n")
+    if evil_status == "Yes":
+        print("You're not welcome here Evil Ben!! Get out!!")
+        exit()
+    else:
+        print("Oh, so you're one of those good Bens. Come on in!!")
 else:
     print("Hello " + name + ", thank you so much for coming in today.\n\n")
-
 
 #Pizza Menu
 menu = "Personal Pizza, Medium Pizza, Large Pizza, XL Pizza"
 
-print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
+#Ask customer what they want from menu
+order = input(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu + "\n")
 
-order = input()
-
-price = 12
-
+#Ask customer how many pizzas they want
 quantity = input("How many pizzas would you like?\n")
+
+#Set the price for pizza
+
+if order  == "XL Pizza":
+    price = 20
+elif order == "Large Pizza":
+    price = 16
+elif order == "Medium Pizza":
+    price = 12
+elif order == "Personal Pizza":
+    price = 8
+else:
+    print("Sorry, we don't have that here.")
+    price = 0
+
+print(price)
 
 total = price * int(quantity)
 
