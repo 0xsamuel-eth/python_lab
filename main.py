456ed5144ac9c57bff97dc3c2382b9af7aac06a9
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-11 22:56:11 -0400

    Added print statements and comments

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # hey....there is nothing here. WAKE UP!!!
 
 print("Hello World!!!!!!!!!!!")
-
+print('I am Iron Man')
+print("No, I am Tony Stark")
+print("No, I am Poppy")
+# I am practicing Python...
