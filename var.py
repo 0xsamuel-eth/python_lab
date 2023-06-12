93e45d5f707c14a5ffcd340fb7161ee2907ee563
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-12 10:07:47 -0400

    Changes to variables and inputs file

---
+++ var.py
@@ -0,0 +1,17 @@
+#Let's start a pizza parlor together! We're going to build a pizza parlor using some new Python programming concepts!!
+
+#Let's build robot Cashier
+
+print("\nHello, welcome to 0xSamuel Pizza!!!!!!!\n")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thank you so much for coming in today.\n\n\n")
+
+menu = "Personal Pizza, Medium Pizza, Large Pizza, XL Pizza"
+
+print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
+
+order = input()
+
+print("\nSounds good " + name + ", we'll have that " + order + " ready for you in 10-15 minutes.")
