e43c9101c1c4607c30c97510fbe43cdbe7d387ab
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-11 23:01:21 -0400

    Multi-line strings implemented

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # hey....there is nothing here. WAKE UP!!!
 
-print("Hello World!!!!!!!!!!!")
-print('I am Iron Man')
-print("No, I am Tony Stark")
-print("No, I am Poppy")
-# I am practicing Python...
+#print("Hello World!!!!!!!!!!!")
+#print('I am Iron Man')
+#print("No, I am Tony Stark")
+#rint("No, I am Poppy")
+#I am practicing Python...
+#
+
+print(""" I am Iron Man. 
+No, I am Tony Stark. 
+No, I am Poppy.""")
