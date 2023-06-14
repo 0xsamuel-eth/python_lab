d47ae759d04a27ed01d43f369ed36362ff442755
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 16:16:44 -0400

    Created list of strings for supplies

--- lists.py
+++ lists.py
@@ -1,6 +1,16 @@
-# Hey, we're going on a camping trip!!
+#Hey, we're going on a camping trip!!
 
 #here is the stuff we need
 
 #tent, sleeping bags,water, raspberry pi, coffee, knife, ethernet cable, flash drive, beard oil, marshmallows
+
+camping_stuff = "tent, sleeping bags,water, raspberry pi, coffee, knife, ethernet cable, flash drive, beard oil, marshmallows"
+
+print(camping_stuff)
+
+#PYTHON LIST
+
+camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+print(type(camping_stuff))
 
