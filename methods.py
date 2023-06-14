f7623676b08326e1bbc577b37a43ca7ca501d6ad
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 16:52:14 -0400

    Appended list to include additional string

--- methods.py
+++ methods.py
@@ -1,6 +1,13 @@
 #Let's learn about lists and methods
 
+#What is a Python Method?
+#A function that is available for a given object b/c of the object's type
+
 supplies = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
 
 camp_site = ["Crystal Lake", 404, 89.3, True]
+
+supplies.append("toilet paper")
+
+print(supplies)
 
