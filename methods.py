d1fdb749f2f1d58760180a754582e024b0457f54
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 16:49:16 -0400

    Created workspace for lists and methods

---
+++ methods.py
@@ -0,0 +1,6 @@
+#Let's learn about lists and methods
+
+supplies = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+camp_site = ["Crystal Lake", 404, 89.3, True]
+
