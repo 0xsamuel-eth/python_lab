a586e801237035bc90df1eca51997381a167a58c
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 16:12:19 -0400

    Created workspace for lists

---
+++ lists.py
@@ -0,0 +1,6 @@
+# Hey, we're going on a camping trip!!
+
+#here is the stuff we need
+
+#tent, sleeping bags,water, raspberry pi, coffee, knife, ethernet cable, flash drive, beard oil, marshmallows
+
