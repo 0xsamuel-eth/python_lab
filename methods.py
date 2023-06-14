d901f4f31df9c71ad2b827a4e003ce50c153670c
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 17:00:39 -0400

    Adjusted list using insert method

--- methods.py
+++ methods.py
@@ -5,9 +5,16 @@
 
 supplies = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
 
-camp_site = ["Crystal Lake", 404, 89.3, True]
+camp_site = ["Crystal Lake", 404, 95.5, 10, False]
 
-supplies.append("toilet paper")
+#supplies.append("toilet paper")
+
+#supplies.extend("toilet paper", "bidet")
+
+supplies.insert(0, "bidet")
+
+#Add an item to second to last location in list
+supplies.insert(-1, "toilet paper")
 
 print(supplies)
 
