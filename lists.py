3bdb973718b2c4428c7ff1e405123a89fd2452b7
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 16:24:51 -0400

    Added commands to index into lists and data types

--- lists.py
+++ lists.py
@@ -12,5 +12,17 @@ print(camping_stuff)
 
 camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
 
-print(type(camping_stuff))
+#List with 4 different data types
+camp_site = ["Crystal Lake", 404, 89.3, True]
 
+#list order starts at 0
+me = camping_list[4]
+#last item in list is -1 then counts in reverse
+you = camping_list[-2]
+
+
+
+print(me)
+print(you)
+
+print(camping_list)
