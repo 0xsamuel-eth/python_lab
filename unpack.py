2446c20eef335b8bd617ecd59af4bd1257c79625
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 17:29:38 -0400

    Unpacked tuple with variable names

---
+++ unpack.py
@@ -0,0 +1,13 @@
+#Unpacking a Tuple
+
+networkchuck = ("Chuck", 33, "coffee")
+
+(name, age, drink) = networkchuck
+
+print(name)
+print(age)
+print(drink)
+
+totallyatuple = 1,
+
+print(type(totallyatuple))
