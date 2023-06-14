9f063142849ffcdcb17e94ca1cb3ee8ac3ffa40c
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-14 16:04:51 -0400

    Created workspace for logical operators

---
+++ logical.py
@@ -0,0 +1,10 @@
+print("Hello, welcome to 0xSamuel Pizza!!!!!!!")
+
+
+name = input("What is your name?\n")
+
+
+if not name == "Ben":
+    print("Come on in!")
+else:
+    print("Get out of here!!")
